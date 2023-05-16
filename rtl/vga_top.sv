5955a84c7ceea3b18be6f23fc9134700dfed4593
alexkharl <sasha_xarlamov@mail.ru>
2023-05-16 19:23:46 +0300

    commit

--- rtl/vga_top.sv
+++ rtl/vga_top.sv
@@ -1,9 +1,13 @@
 module vga_top(
-  input clk, arstn,
+  input clk_i, arstn_i,
   
-  output VGA_HS, VGA_VS,
-  output [11:0] RGB,
-  output [11:0] LED
+  output VGA_HS_o, VGA_VS_o,
+  input  color_i,
+  input [10:0] addr_x_i,
+  input [10:0] addr_y_i,
+  input        we_i,
+  output [11:0] RGB_o,
+  output [11:0] LED_o
 );
   localparam HSYNC_BITS = 11,
              VSYNC_BITS = 11,
@@ -13,7 +17,7 @@ module vga_top(
   localparam logic [11:0] WHITE = '1; 
   localparam logic [11:0] BLACK = '0; 
 
-  logic [11:0] SW;
+  logic [11:0] color_ff;
   logic [HSYNC_BITS-1:0] hcount;
   logic [VSYNC_BITS-1:0] vcount;
 
@@ -23,23 +27,29 @@ module vga_top(
     .HD( HD ),
     .VD( VD )
   ) vga(
-    .clk( clk ), .arstn( arstn ),
+    .clk( clk_i ), .arstn( arstn_i ),
     
-    .SW( SW ),
+    .SW( color_ff ),
     
-    .VGA_HS( VGA_HS ), .VGA_VS( VGA_VS ),
-    .RGB( RGB ),
-    .LED( LED ),
+    .VGA_HS( VGA_HS_o ), .VGA_VS( VGA_VS_o ),
+    .RGB( RGB_o ),
+    .LED( LED_o ),
     
     .hcount( hcount ),
     .vcount( vcount )
   );
 
-  logic video_buffer[VD][HD];
-
-  always_ff @( posedge clk )
-    if( vcount == 1000 ) SW <= WHITE;
-    else                 SW <= BLACK;
-
+  logic video_buffer_ff[VD * HD];
+  
+  logic video_buffer_pixel_ff;
+  
+  always_ff @( posedge clk_i ) 
+    if( we_i )  video_buffer_ff[addr_x_i * HD + addr_y_i] <= color_i;
+    
+  always_ff @( posedge clk_i )
+    video_buffer_pixel_ff <= video_buffer_ff[vcount * HD + hcount];
+  
+  always_ff @( posedge clk_i )
+    color_ff <= { 12{video_buffer_pixel_ff} };
 
 endmodule
