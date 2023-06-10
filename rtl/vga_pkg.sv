1fbb7d875c6d20bbfa42ac3fea2afa230949d6b4
alexkharl <sasha_xarlamov@mail.ru>
2023-06-10 13:42:55 +0300

    Fix(pkg):Исправление надостаточной ширины

--- rtl/vga_pkg.sv
+++ rtl/vga_pkg.sv
@@ -1,8 +1,8 @@
 package vga_pkg;
-  localparam VGA_MAX_H = 1280;
-  localparam VGA_MAX_V = 1280;
+  localparam VGA_MAX_H = 1280; // maximum supported resolution in pixels
+  localparam VGA_MAX_V = 1024;
 
-  localparam VGA_MAX_H_WIDTH = $clog2( VGA_MAX_H );
-  localparam VGA_MAX_V_WIDTH = $clog2( VGA_MAX_V );
+  localparam VGA_MAX_H_WIDTH = $clog2( VGA_MAX_H * 2 );  // extra width for HF + HR + HB
+  localparam VGA_MAX_V_WIDTH = $clog2( VGA_MAX_V * 2 );
   
 endpackage
