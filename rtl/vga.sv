51493d4ba5d46da74fd29cacc7decfd932513ce3
alexkharl <sasha_xarlamov@mail.ru>
2023-06-13 19:11:32 +0300

    Ref(vga):Переписывание vga,перестановка областей
    Активная область должна быть вначале, чтобы не пришлось преобразовывать
    адрес памяти в буфере кадра.

--- rtl/vga.sv
+++ rtl/vga.sv
@@ -29,14 +29,14 @@ module vga
   output logic                       pixel_enable_o
 
 );
-  logic  [VGA_MAX_H_WIDTH-1:0] hmax_ff;
-  logic  [VGA_MAX_H_WIDTH-1:0] hmax_next;
+  logic  [VGA_MAX_H_WIDTH-1:0] htotal_ff;
+  logic  [VGA_MAX_H_WIDTH-1:0] htotal_next;
 
-  logic  [VGA_MAX_V_WIDTH-1:0] vmax_ff;
-  logic  [VGA_MAX_V_WIDTH-1:0] vmax_next;
+  logic  [VGA_MAX_V_WIDTH-1:0] vtotal_ff;
+  logic  [VGA_MAX_V_WIDTH-1:0] vtotal_next;
 
-  assign hmax_next = hd_i + hf_i + hr_i + hb_i - 1; // MAX counter value
-  assign vmax_next = vd_i + vf_i + vr_i + vb_i - 1;
+  assign htotal_next = hd_i + hf_i + hr_i + hb_i;
+  assign vtotal_next = vd_i + vf_i + vr_i + vb_i;
   
   // Sync signal registers, vertical counter enable register, and pixel enable register
   logic hsync_ff;
@@ -84,8 +84,8 @@ module vga
       vr_ff <= '0;
       vb_ff <= '0;
 
-      hmax_ff <= '0;
-      vmax_ff <= '0;
+      htotal_ff <= '0;
+      vtotal_ff <= '0;
     end else if ( we_i     ) begin
       hd_ff <= hd_i;
       hf_ff <= hf_i;
@@ -97,12 +97,12 @@ module vga
       vr_ff <= vr_i;
       vb_ff <= vb_i;
 
-      hmax_ff <= hmax_next;
-      vmax_ff <= vmax_next;
+      htotal_ff <= htotal_next;
+      vtotal_ff <= vtotal_next;
     end
   
   // Horizontal counter
-  assign hcount_en   = hcount_ff < hmax_ff;
+  assign hcount_en   = hcount_ff < ( htotal_ff - 1 );
   assign hcount_next = hcount_ff + 1;
   always_ff @ ( posedge clk_i or negedge arstn_i )
     if      ( ~arstn_i  ) hcount_ff <= '0;
@@ -110,15 +110,15 @@ module vga
     else                  hcount_ff <= '0;
   
   // Vertical counter
-  assign vcount_en   = ( hcount_ff == hmax_ff );
-  assign vcount_next = ( vcount_ff < vmax_ff ) ? ( vcount_ff + 1 ) : ( '0 );
+  assign vcount_en   = ( hcount_ff == ( htotal_ff - 1 ) );
+  assign vcount_next = ( vcount_ff < ( vtotal_ff - 1 ) ) ? ( vcount_ff + 1 ) : ( '0 );
   always_ff @( posedge clk_i or negedge arstn_i ) 
     if      ( ~arstn_i    ) vcount_ff <= '0;
     else if ( vcount_en   ) vcount_ff <= vcount_next;
   
   // Horizontal and Vertical sync signal generator
-  assign hsync_next = (hcount_ff < hr_ff) ? 1'b1 : 1'b0;
-  assign vsync_next = (vcount_ff < vr_ff) ? 1'b1 : 1'b0;
+  assign hsync_next = ( hcount_ff < ( hd_ff + hf_ff - 1 ) || hcount_ff > ( hd_ff + hf_ff + hr_ff - 1 ) ) ? 1'b1 : 1'b0;
+  assign vsync_next = ( vcount_ff < ( vd_ff + vf_ff - 1 ) || vcount_ff > ( vd_ff + vf_ff + vr_ff - 1 ) ) ? 1'b1 : 1'b0;
   always_ff @ ( posedge clk_i or negedge arstn_i )
     if( ~arstn_i ) begin
       hsync_ff <= '0;
@@ -132,7 +132,7 @@ module vga
   assign vga_hs_o = hsync_ff;
   assign vga_vs_o = vsync_ff;
   
-  assign pixel_enable_en = hcount_ff >= (hr_ff+hb_ff) && hcount_ff < (hr_ff+hb_ff+hd_ff) && vcount_ff >= (vr_ff+vb_ff) && vcount_ff < (vr_ff+vb_ff+vd_ff);
+  assign pixel_enable_en = hcount_ff < ( hd_ff - 1) && vcount_ff < ( vd_ff - 1 );
   assign pixel_enable_o  = pixel_enable_ff;
   always_ff @( posedge clk_i or negedge arstn_i ) 
     if      ( ~arstn_i        ) pixel_enable_ff <= '0;
