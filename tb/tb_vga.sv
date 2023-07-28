90a6b016060431a0872cf29c9af562e5af0f1305
alexkharl <sasha_xarlamov@mail.ru>
2023-07-28 15:59:32 +0300

    Feat(tb_vga):Подключение ddr

--- tb/tb_vga.sv
+++ tb/tb_vga.sv
@@ -29,13 +29,66 @@ logic [11:0] RGB;
 
 logic clk, arstn;
 logic [2:0] sw;
-    
+
+  // Inouts
+
+  wire [15:0]                       ddr2_dq;
+  wire [1:0]                        ddr2_dqs_n;
+  wire [1:0]                        ddr2_dqs_p;
+
+    // Outputs
+
+  wire [12:0]                      ddr2_addr;
+  wire [2:0]                       ddr2_ba;
+  wire                             ddr2_ras_n;
+  wire                             ddr2_cas_n;
+  wire                             ddr2_we_n;
+  wire [0:0]                       ddr2_ck_p;
+  wire [0:0]                       ddr2_ck_n;
+  wire [0:0]                       ddr2_cke;
+  wire [1:0]                       ddr2_dm;
+  wire [0:0]                       ddr2_odt;
+
+  ddr2_model ddr2_model (
+    .ck( ddr2_ck_p ),
+    .ck_n( ddr2_ck_n ),
+    .cke( ddr2_cke ),
+    .cs_n( 1'b0 ),
+    .ras_n( ddr2_ras_n ),
+    .cas_n( ddr2_cas_n ),
+    .we_n( ddr2_we_n ),
+    .dm_rdqs( ddr2_dm ),
+    .ba( ddr2_ba ),
+    .addr( ddr2_addr ),
+    .dq( ddr2_dq ),
+    .dqs( ddr2_dqs_p ),
+    .dqs_n( ddr2_dqs_n ),
+    .rdqs_n(  ),
+    .odt( ddr2_odt )
+  );
+
   vga_top_top vga_top_top(
     .clk_i( clk ), .arstn_i( arstn ),
     .sw( sw ),
 
     .VGA_HS_o( VGA_HS ), .VGA_VS_o( VGA_VS ),
-    .RGB_o( RGB )
+    .RGB_o( RGB ),
+    // Inouts
+
+    .ddr2_dq,
+    .ddr2_dqs_n,
+    .ddr2_dqs_p,
+    //( // ) Outputs
+    .ddr2_addr,
+    .ddr2_ba,
+    .ddr2_ras_n,
+    .ddr2_cas_n,
+    .ddr2_we_n,
+    .ddr2_ck_p,
+    .ddr2_ck_n,
+    .ddr2_cke,
+    .ddr2_dm,
+    .ddr2_odt
   );
 		
     initial begin
